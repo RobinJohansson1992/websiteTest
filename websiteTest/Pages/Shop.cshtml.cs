8b03287c8dc6a6ba27c5e127507d26191df2e0b0
RobinJohansson1992 <robin-johansson@live.com>
2026-01-01 22:05:15 +0100

    Add project files.

---
+++ websiteTest/Pages/Shop.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace websiteTest.Pages
+{
+    public class ShopModel : PageModel
+    {
+        [BindProperty]
+        public string NewItem { get; set; }
+        public static List<string> ItemList { get; set; } = new();
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!string.IsNullOrWhiteSpace(NewItem))
+            {
+                ItemList.Add(NewItem);
+                NewItem = string.Empty;
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostRemove(int index)
+        {
+            if (index >= 0 && index < ItemList.Count)
+            {
+                ItemList.RemoveAt(index);
+            }
+            return Page();
+        }
+    }
+}
---
+++ websiteTest/Pages/Work.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace websiteTest.Pages
+{
+    public class WorkModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
